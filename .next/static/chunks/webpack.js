be5c65876d0e0dc899b7ae28444bdda43091dbb5
Jake Akerley <jakeak6@gmail.com>
2025-07-24 12:45:47 -0600

    Improve mobile responsiveness and layout across the site

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2a7f4c13e737baa"; }
+/******/ 		__webpack_require__.h = function() { return "2367a5cb59ef2117"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/Home.module.css';
 import '../styles/Info.module.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 } 
